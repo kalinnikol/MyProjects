460ad438820e8d348985ec79cef3ef61a4fe6862
Lina Chalakova <chalakovalina@gmail.com>
2013-08-26 16:30:10 +0300

    WinJS Pages and navigation homework

---
+++ WinJsPagesAndNavigationHomework/SofiaApplication/pages/home/data.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+
+    var dataArray = [
+    { title: "History", text: "Click to read more", picture: "images/architecture.jpg" },
+    { title: "Culture", text: "Click to read more", picture: "images/architecture.jpg" },
+    { title: "Architecture", text: "Click to read more", picture: "images/architecture.jpg" }
+    ];
+
+    var dataList = new WinJS.Binding.List(dataArray);
+    var onPageClicked = function () {
+        console.log(event);
+    }
+
+    // Create a namespace to make the data publicly
+    // accessible. 
+    var publicMembers =
+        {
+            itemList: dataList,
+            onPageClicked:onPageClicked
+        };
+    WinJS.Namespace.define("Data", publicMembers);
+
+})();
---
+++ WinJsPagesAndNavigationHomework/SofiaApplication/pages/home/home.js
@@ -0,0 +1,45 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+/// <reference path="data.js" />
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+       
+        ready: function (element, options) {
+
+            var listView = document.getElementById("basicListView").winControl;
+         
+            listView.oniteminvoked = function (event) {
+               
+                var title = Data.itemList.getAt(event.detail.itemIndex).title;
+
+                if (title=="History") {
+                    goToHistory();
+                }
+                else if (title == "Culture") {
+                    goToCulture();
+                }
+                else if (title=="Architecture") {
+                    goToArchitecture();
+                }
+            };
+        }
+    });
+})();
+
+var goToHistory = function () {
+    WinJS.Navigation.navigate("ms-appx:///pages/history/history.html");
+
+};
+
+var goToCulture = function () {
+    WinJS.Navigation.navigate("ms-appx:///pages/culture/culture.html");
+
+};
+
+var goToArchitecture = function () {
+    WinJS.Navigation.navigate("ms-appx:///pages/architecture/architecture.html");
+};
+
