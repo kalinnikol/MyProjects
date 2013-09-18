f2f0a29b42b127b7180dbc191368b25687d9310e
Lina Chalakova <chalakovalina@gmail.com>
2013-09-18 23:55:29 +0300

    AsynchronousProgramming Homework
    Win8 with XAML

---
+++ AsynchronousProgrammingHomework/PrimeNumbersOperations/Library/PrimesOperator.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbersOperations.Library
+{
+    public class PrimesOperator
+    {
+        public async Task<string> FindPrimeOrNotInPrimesConcatenationsAsync(long rangeFirst, long rangeLast, bool findPrimes)
+        {
+            List<long> concatenatedPrimes = await MakePrimesConcatenationsAsync(rangeFirst, rangeLast);
+
+            List<long> resultList = new List<long>();
+            if (findPrimes)
+            {
+                await AddPrimesToListAsync(concatenatedPrimes, resultList);
+            }
+            else
+            {
+                await AddNotPrimesToListAsync(concatenatedPrimes, resultList);
+            }
+
+            string resultString = ConcatenatePrimes(resultList);
+
+            return resultString;
+        }
+  
+        private  void AddNotPrimesToList(List<long> concatenatedPrimes, List<long> resultList)
+        {
+            foreach (var concatenatedNumber in concatenatedPrimes)
+            {
+                bool isNotPrime = FindIfIsNotPrime(concatenatedNumber);
+                if (isNotPrime)
+                {
+                    resultList.Add(concatenatedNumber);
+                }
+            }
+        }
+
+        private Task AddNotPrimesToListAsync(List<long> concatenatedPrimes, List<long> resultList)
+        {
+            return Task.Run(() => AddNotPrimesToList(concatenatedPrimes, resultList));
+        }
+
+        private  void AddPrimesToList(List<long> concatenatedPrimes, List<long> resultList)
+        {
+            foreach (var concatenatedNumber in concatenatedPrimes)
+            {
+                bool isPrime = FindIfPrime(concatenatedNumber);
+                if (isPrime)
+                {
+                    resultList.Add(concatenatedNumber);
+                }
+            }
+        }
+
+        private  Task AddPrimesToListAsync(List<long> concatenatedPrimes, List<long> resultList)
+        {
+            return Task.Run(()=>AddPrimesToList(concatenatedPrimes, resultList));
+        }
+
+        private  bool FindIfPrime(long number)
+        {
+            bool isPrime = true;
+            for (long divider = 2; divider < number; divider++)
+            {
+                if (number % divider == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            return isPrime;
+        }
+
+
+        private bool FindIfIsNotPrime(long number)
+        {
+            bool isNotPrime = true;
+            for (long divider = 2; divider < number; divider++)
+            {
+                if (number % divider != 0)
+                {
+                    isNotPrime = false;
+                    break;
+                }
+            }
+            return isNotPrime;
+        }
+
+        private  Task<List<long>> GetPrimesInRangeAsync(long rangeFirst, long rangeLast)
+        {
+            return Task.Run(
+                () => GetPrimesInRange(rangeFirst, rangeLast)
+                    );
+        }
+
+        private  List<long> GetPrimesInRange(long rangeFirst, long rangeLast)
+        {
+            List<long> primes = new List<long>();
+
+            for (long number = rangeFirst; number < rangeLast; number++)
+            {
+                bool isPrime = FindIfPrime(number);
+                if (isPrime)
+                {
+                    primes.Add(number);
+                }
+            }
+
+            return primes;
+        }
+
+        private  async Task <List<long>> MakePrimesConcatenationsAsync(long rangeFirst, long rangeLast)
+        {
+            List<long> primesList = await GetPrimesInRangeAsync(rangeFirst, rangeLast);
+
+            List<long> primesConcatenations = new List<long>();
+
+            foreach (var primeNumber in primesList)
+            {
+                string primeStr = primeNumber.ToString();
+                char firstDigit = primeStr[0];
+
+                foreach (var primeNumberInner in primesList)
+                {
+                    string primeInnerStr = primeNumberInner.ToString();
+                    char secondDigit = primeInnerStr[primeInnerStr.Length - 1];
+
+                    if (firstDigit == secondDigit)
+                    {
+                        int newNumber = int.Parse(primeStr + primeInnerStr);
+                        primesConcatenations.Add(newNumber);
+                    }
+                }
+            }
+
+            return primesConcatenations;
+        }
+
+        private  string ConcatenatePrimes(List<long> resultList)
+        {
+            string result = JoinList(resultList);
+
+            return result;
+        }
+
+        private  string JoinList(List<long> resultList)
+        {
+            return string.Join(", ", resultList);
+        }
+    }
+}
---
+++ AsynchronousProgrammingHomework/PrimeNumbersOperations/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using PrimeNumbersOperations.Library;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PrimeNumbersOperations
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void OnFirstButtonClick(object sender, RoutedEventArgs e)
+        {
+            long lowerRange = long.Parse(LowerRangeFirst.Text);
+            long upperRange = long.Parse(UpperRangeFirst.Text);
+            bool prime = FirstToggle.IsOn;
+            PrimesOperator oper1=new PrimesOperator();
+            string result = await oper1.FindPrimeOrNotInPrimesConcatenationsAsync(lowerRange, upperRange, prime);
+            if (string.IsNullOrEmpty(result))
+            {
+                result = "No numbers found";
+            }
+            ResultBlockFirst.Text = result;
+        }
+
+        private async void OnSecondButtonClick(object sender, RoutedEventArgs e)
+        {
+            int lowerRange = int.Parse(LowerRangeSecond.Text);
+            int upperRange = int.Parse(UpperRangeSecond.Text);
+            bool prime = SecondToggle.IsOn;
+            PrimesOperator oper2 = new PrimesOperator();
+            string result = await oper2.FindPrimeOrNotInPrimesConcatenationsAsync(lowerRange, upperRange, prime);
+            if (string.IsNullOrEmpty(result))
+            {
+                result = "No numbers found";
+            }
+            ResultBlockSecond.Text = result;
+        }
+
+        private async void OnThirdButtonClick(object sender, RoutedEventArgs e)
+        {
+            int lowerRange = int.Parse(LowerRangeThird.Text);
+            int upperRange = int.Parse(UpperRangeThird.Text);
+            bool prime = ThirdToggle.IsOn;
+            PrimesOperator oper3 = new PrimesOperator();
+            string result = await oper3.FindPrimeOrNotInPrimesConcatenationsAsync(lowerRange, upperRange, prime);
+            if (string.IsNullOrEmpty(result))
+            {
+                result = "No numbers found";
+            }
+            ResultBlockThird.Text = result;
+        }
+
+        private async void OnForthButtonClick(object sender, RoutedEventArgs e)
+        {
+            int lowerRange = int.Parse(LowerRangeForth.Text);
+            int upperRange = int.Parse(UpperRangeForth.Text);
+            bool prime = ForthToggle.IsOn;
+            PrimesOperator oper4 = new PrimesOperator();
+            string result = await oper4.FindPrimeOrNotInPrimesConcatenationsAsync(lowerRange, upperRange, prime);
+            if (string.IsNullOrEmpty(result))
+            {
+                result = "No numbers found";
+            }
+            ResultBlockForth.Text = result;
+        }
+    }
+}
